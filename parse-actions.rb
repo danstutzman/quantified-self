632499e904ed5a447e9827231c4164f680976cc4
Daniel Stutzman <dtstutz@gmail.com>
2012-05-28 11:47:46 -0600

    Refactor to use Log class instead of 3-element tuple.

--- parse-actions.rb
+++ parse-actions.rb
@@ -16,6 +16,13 @@ else
   exit 1
 end
 
+class Log
+  attr_reader :start, :finish, :comment
+  def initialize(*args)
+    @start, @finish, @comment = args
+  end
+end
+
 lines = []
 File.open(File.expand_path('../actions.txt', __FILE__)) { |file|
   file.each_line { |line|
@@ -43,12 +50,12 @@ lines.each { |line|
   in_pm = time.match(/^(1[0-9]|2[0-3])/)
 
   if comment == 'no'
-    if logs.last[2].nil?
+    if logs.last.comment.nil?
       logs.pop # remove last log
     end
     next
   elsif comment.match(/^actually (.*)$/)
-    if logs.last[2].nil?
+    if logs.last.comment.nil?
       logs.pop # remove last log
     end
     comment = $1
@@ -62,47 +69,44 @@ lines.each { |line|
   end
 
   am_on_break = false
-  start_time, end_time, activity =
-    case comment
-      when 'now what?'
-        am_on_break = true
-        [time, nil, comment]
-      when /^(start on|start to|start|now|switch to) (.*)$/
-        [time, nil, $2]
-      when 'back'
-        if just_was_on_break
-          [time, nil, logs.last && logs.last[2]]
-        else
-          [nil, time, logs.last && logs.last[2]]
-        end
-      when /^(done with that|done|great|break|take a break)$/
-        am_on_break = true
-        [nil, time, logs.last && logs.last[2]]
-      when /^(was|done|done with|break from) (.*)$/
-        am_on_break = true
-        [nil, time, $2]
-      when /^(off the phone with) (.*)$/
-        [nil, time, "phone call with #{$2}"]
+  new_log = case comment
+    when 'now what?'
+      Log.new(time, nil, comment)
+    when /^(start on|start to|start|now|switch to) (.*)$/
+      Log.new(time, nil, $2)
+    when 'back'
+      if just_was_on_break
+        Log.new(time, nil, logs.last && logs.last.comment)
       else
-        [time, nil, comment]
-    end
-  new_log = [start_time, end_time, activity]
+        Log.new(nil, time, logs.last && logs.last.comment)
+      end
+    when /^(done with that|done|great|break|take a break)$/
+      am_on_break = true
+      Log.new(nil, time, logs.last && logs.last.comment)
+    when /^(was|done|done with|break from) (.*)$/
+      am_on_break = true
+      Log.new(nil, time, $2)
+    when /^(off the phone with) (.*)$/
+      Log.new(nil, time, "phone call with #{$2}")
+    else
+      Log.new(time, nil, comment)
+  end
   logs.push new_log
 
   just_was_on_break = am_on_break
 }
 
 logs.sort! { |log1, log2|
-  (log1[0] || log1[1]) <=> (log2[0] || log2[1])
+  (log1.start || log1.finish) <=> (log2.start || log2.finish)
 }
 
 new_logs = []
 logs.each { |log|
-  if log[0].nil? &&
-     new_logs.last && new_logs.last[1].nil? &&
-     log[2] == new_logs.last[2]
+  if log.start.nil? &&
+     new_logs.last && new_logs.last.finish.nil? &&
+     log.comment == new_logs.last.comment
     last_log = new_logs.pop # remove last log
-    new_logs.push [last_log[0], log[1], log[2]]
+    new_logs.push Log.new(last_log.start, log.finish, log.comment)
   else
     new_logs.push log
   end
@@ -110,5 +114,5 @@ logs.each { |log|
 logs = new_logs
 
 logs.each { |log|
-  puts sprintf('%-10s %-10s %s', log[0], log[1], log[2])
+  puts sprintf('%-10s %-10s %s', log.start, log.finish, log.comment)
 }
