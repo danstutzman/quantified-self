f40b3853d660d1c36f1f226baa97067bc1a3bf42
Daniel Stutzman <dtstutz@gmail.com>
2012-05-28 12:10:54 -0600

    Oops, rename of notes.txt to actions.txt broke script.

--- nag-about-lack-of-notes.rb
+++ nag-about-lack-of-notes.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 require 'parsedate'
 
-notes_path = File.expand_path('../notes.txt', __FILE__)
+notes_path = File.expand_path('../actions.txt', __FILE__)
 last_timestamp = `tail -1 #{notes_path}`.split("\t")[0]
 last_time = Time.local(*ParseDate.parsedate(last_timestamp))
 seconds_elapsed = Time.new.to_i - last_time.to_i
