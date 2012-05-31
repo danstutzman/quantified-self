1ec2950e8f29f8742b547bde2c15e0988db74497
Daniel Stutzman <dtstutz@gmail.com>
2012-05-30 20:36:27 -0600

    Prefix all weather metrics keys with weather- so they are grouped together.

--- yahoo-weather.rb
+++ yahoo-weather.rb
@@ -19,17 +19,17 @@ result_set = REXML::Document.new(xml.body)
 out = {}
 result_set.elements.each('rss/channel') do |element|
   element.elements.each('yweather:atmosphere') do |child|
-    out['humidity'] = safe { child.attributes['humidity'].to_i }
-    out['pressure'] = safe { child.attributes['pressure'].to_f }
+    out['weather-humidity'] = safe { child.attributes['humidity'].to_i }
+    out['weather-pressure'] = safe { child.attributes['pressure'].to_f }
     break
   end
   element.elements.each('yweather:astronomy') do |child|
-    out['sunrise-hour'] = safe {
+    out['weather-sunrise-hour'] = safe {
       parts = ParseDate.parsedate(child.attributes['sunrise'])
       hours, minutes = parts[3], parts[4]
       sprintf('%.3f', hours + (minutes / 60.0))
     }
-    out['sunset-hour'] = safe {
+    out['weather-sunset-hour'] = safe {
       parts = ParseDate.parsedate(child.attributes['sunset'])
       hours, minutes = parts[3], parts[4]
       sprintf('%.3f', hours + (minutes / 60.0))
@@ -38,9 +38,9 @@ result_set.elements.each('rss/channel') do |element|
   end
   element.elements.each('item') do |item|
     item.elements.each('yweather:forecast') do |child|
-      out['temperature-high'] = safe { child.attributes['high'].to_i }
-      out['temperature-low'] = safe { child.attributes['low'].to_i }
-      out['condition-code'] = safe { child.attributes['code'].to_i }
+      out['weather-temperature-high'] = safe { child.attributes['high'].to_i }
+      out['weather-temperature-low'] = safe { child.attributes['low'].to_i }
+      out['weather-condition-code'] = safe { child.attributes['code'].to_i }
       break
     end
     break
