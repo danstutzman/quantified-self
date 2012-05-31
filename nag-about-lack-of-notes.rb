ac5cdaf42bdee79524782966d9402106deb403c6
Daniel Stutzman <dtstutz@gmail.com>
2012-05-30 20:35:27 -0600

    Lock file isn't working for nag-about-lock-of-notes because the popup apparently times out.

--- nag-about-lack-of-notes.rb
+++ nag-about-lack-of-notes.rb
@@ -5,14 +5,8 @@ notes_path = File.expand_path('../actions.txt', __FILE__)
 last_timestamp = `tail -1 #{notes_path}`.split("\t")[0]
 last_time = Time.local(*ParseDate.parsedate(last_timestamp))
 seconds_elapsed = Time.new.to_i - last_time.to_i
-lock_path = File.expand_path('../nag-about-lack-of-notes.lock', __FILE__)
 script_path = File.expand_path('../nag-about-lack-of-notes.sh', __FILE__)
 if seconds_elapsed > 60 * 60
-  if File.exists?(lock_path)
-    # don't popup a new dialog
-  else
-    `touch #{lock_path}`
-    `bash -c '#{script_path} #{last_time.strftime('%l:%M%p')}'`
-    # script will remove the lock file
-  end
+  `touch #{lock_path}`
+  `bash -c '#{script_path} #{last_time.strftime('%l:%M%p')}'`
 end
--- nag-about-lack-of-notes.sh
+++ nag-about-lack-of-notes.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 /usr/bin/osascript <<END
   tell app "Finder" to display dialog "What have you been doing since $1? Please press Ctrl-Shift-A to record a new action."
-  do shell script "rm /Users/dstutzman/Documents/quantified-self/nag-about-lack-of-notes.lock"
 END
