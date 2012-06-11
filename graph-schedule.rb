cf7d132908076b6f4b8de2f1603119204b11ed29
Daniel Stutzman <dtstutz@gmail.com>
2012-06-10 18:26:50 -0600

    Refactor graph-schedule, make it able to detect how wide chart should be.

--- graph-schedule.rb
+++ graph-schedule.rb
@@ -7,62 +7,64 @@ Y_SPACE_PER_HOUR = 30
 X_SPACE_PER_DAY = 60
 DAYS_SHOWN = 5
 
-dates = {}
-doc = REXML::Document.new('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" onload="init(evt)"></svg>')
+def add_interactivity(doc)
+  script = doc[0].add_element('script')
+  script.attributes['type'] = 'text/ecmascript'
+  script.text = '
+      function init(evt) {
+          if ( window.svgDocument == null ) {
+              svgDocument = evt.target.ownerDocument;
+          }
+          tooltip = svgDocument.getElementById("tooltip");
+      }
+  
+      function ShowTooltip(evt) {
+          // Put tooltip in the right position, change the text and make it visible
+          tooltip.setAttributeNS(null,"x",evt.clientX+10);
+          tooltip.setAttributeNS(null,"y",evt.clientY+30);
+          tooltip.firstChild.data = evt.target.getAttributeNS(null,"mouseovertext");
+          tooltip.setAttributeNS(null,"visibility","visible");
+      }
+  
+      function HideTooltip(evt) {
+          tooltip.setAttributeNS(null,"visibility","hidden");
+      }
+  '
+end
 
-script = doc[0].add_element('script')
-script.attributes['type'] = 'text/ecmascript'
-script.text = '
-    function init(evt) {
-        if ( window.svgDocument == null ) {
-            svgDocument = evt.target.ownerDocument;
-        }
-        tooltip = svgDocument.getElementById("tooltip");
-    }
+def add_x_axis(doc, num_days)
+  y0 = Time.gm(1970, 1, 1, 0, 0, 0).to_i / (60 * 60) * Y_SPACE_PER_HOUR
+  y1 = Time.gm(1970, 1, 2, 0, 0, 0).to_i / (60 * 60) * Y_SPACE_PER_HOUR
+  (0...num_days).each { |day_num|
+    line = doc[0].add_element('line')
+    line.attributes['x1'] = (day_num + 0.5) * X_SPACE_PER_DAY
+    line.attributes['x2'] = (day_num + 0.5) * X_SPACE_PER_DAY
+    line.attributes['y1'] = y0
+    line.attributes['y2'] = y1
+    line.attributes['style'] = 'stroke:black'
+  }
+end
 
-    function ShowTooltip(evt) {
-        // Put tooltip in the right position, change the text and make it visible
-        tooltip.setAttributeNS(null,"x",evt.clientX+10);
-        tooltip.setAttributeNS(null,"y",evt.clientY+30);
-        tooltip.firstChild.data = evt.target.getAttributeNS(null,"mouseovertext");
-        tooltip.setAttributeNS(null,"visibility","visible");
-    }
-
-    function HideTooltip(evt) {
-        tooltip.setAttributeNS(null,"visibility","hidden");
-    }
-'
-
-y0 = Time.gm(1970, 1, 1, 0, 0, 0).to_i / (60 * 60) * Y_SPACE_PER_HOUR
-y1 = Time.gm(1970, 1, 2, 0, 0, 0).to_i / (60 * 60) * Y_SPACE_PER_HOUR
-midnight = y1
-(0...DAYS_SHOWN).each { |day_num|
-  line = doc[0].add_element('line')
-  line.attributes['x1'] = (day_num + 0.5) * X_SPACE_PER_DAY
-  line.attributes['x2'] = (day_num + 0.5) * X_SPACE_PER_DAY
-  line.attributes['y1'] = y0
-  line.attributes['y2'] = y1
-  line.attributes['style'] = 'stroke:black'
-}
-
-(0...24).each { |hour|
-  next unless hour % 2 == 0
-
-  datetime = Time.gm(1970, 1, 1, hour, 0, 0)
-  y = datetime.to_i / (60 * 60) * Y_SPACE_PER_HOUR
-
-  line = doc[0].add_element('line')
-  line.attributes['x1'] = 0
-  line.attributes['x2'] = X_SPACE_PER_DAY * DAYS_SHOWN
-  line.attributes['y1'] = y
-  line.attributes['y2'] = y
-  line.attributes['style'] = 'stroke:black;stroke-width:0.5'
-
-  text = doc[0].add_element('text')
-  text.text = datetime.strftime('%H:%M')
-  text.attributes['x'] = X_SPACE_PER_DAY * DAYS_SHOWN
-  text.attributes['y'] = y + 5
-}
+def add_y_axis(doc, num_days)
+  (0...24).each { |hour|
+    next unless hour % 2 == 0
+  
+    datetime = Time.gm(1970, 1, 1, hour, 0, 0)
+    y = datetime.to_i / (60 * 60) * Y_SPACE_PER_HOUR
+  
+    line = doc[0].add_element('line')
+    line.attributes['x1'] = 0
+    line.attributes['x2'] = X_SPACE_PER_DAY * num_days
+    line.attributes['y1'] = y
+    line.attributes['y2'] = y
+    line.attributes['style'] = 'stroke:black;stroke-width:0.5'
+  
+    text = doc[0].add_element('text')
+    text.text = datetime.strftime('%H:%M')
+    text.attributes['x'] = X_SPACE_PER_DAY * num_days
+    text.attributes['y'] = y + 5
+  }
+end
 
 def add_rect(doc, day_num, start_time, finish_time, category, comments)
   y0 = start_time.to_f / (60 * 60) * Y_SPACE_PER_HOUR
@@ -87,6 +89,22 @@ def add_rect(doc, day_num, start_time, finish_time, category, comments)
   rect.attributes['onmouseout'] = "HideTooltip(evt)"
   rect.attributes['mouseovertext'] = comments
 end
+
+dates = {}
+CSV.foreach('day-retrospective/logs.csv') { |row|
+  next if row[0].match(/^What /)
+  next if row[0] == 'Date' # column headers
+  date, start, finish, category, comment = row
+  datetime = Time.gm(*ParseDate.parsedate(date))
+  dates[datetime] = true
+}
+num_days = ((dates.keys.max - dates.keys.min) / (24 * 60 * 60.0)) + 2
+
+doc = REXML::Document.new(
+  '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" onload="init(evt)"></svg>')
+add_interactivity(doc)
+add_x_axis(doc, num_days)
+add_y_axis(doc, num_days)
 
 CSV.foreach('day-retrospective/logs.csv') { |row|
   next if row[0].match(/^What /)
@@ -128,6 +146,7 @@ dates.keys.sort.each { |datetime|
 
   g = doc[0].add_element('g')
   x = day_num * X_SPACE_PER_DAY
+  midnight = Time.gm(1970, 1, 2, 0, 0, 0).to_i / (60 * 60) * Y_SPACE_PER_HOUR
   g.attributes['transform'] = "translate(#{x + 0.5}, #{midnight})"
   text = g.add_element('text')
   text.attributes['transform'] = 'rotate(90)'
