b6094b18806864d4b6ee9202c484d55273de7c50
Daniel Stutzman <dtstutz@gmail.com>
2013-02-09 21:07:59 -0700

    First commit.

---
+++ web-app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'pry'
+require 'json'
+
+set :port, 4444
+set :public_folder, 'public'
+set :static_cache_control, [:public, :no_cache]
+
+log = File.open("time.log", "a")
+
+get '/' do
+  erb :page
+end
+
+post '/append-log' do
+  unparsed_json = request.body.read
+  hash = JSON.parse(unparsed_json)
+  puts hash
+  log.puts hash
+  log.flush
+  'OK'
+end
+
+`open http://localhost:4444/`
