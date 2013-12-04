5a4c6a6bd236d9197ca4c4ab482454c9ec7cf363
Daniel Stutzman <dstutzman@dstutzman.(none)>
2013-12-04 15:16:24 -0700

    Storing dell-mini-time-minder in Git.

---
+++ dell-mini-time-minder/turn-off-backlight-server.py
@@ -0,0 +1,23 @@
+import BaseHTTPServer
+import os
+import datetime
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write("ok")
+    self.wfile.close()
+    now = datetime.datetime.now()
+    num_seconds = (5 - now.hour) * 3600 + (59 - now.minute) * 60
+    if num_seconds < 0:
+      num_seconds += 24 * 60 * 60
+    os.system('sleep 1 && xset dpms force off && sleep %d && xset dpms force on' % num_seconds)
+
+
+try:
+  server = BaseHTTPServer.HTTPServer(('localhost', 8000), MyHandler)
+  server.serve_forever()
+except KeyboardInterrupt:
+  server.socket.close()
+
