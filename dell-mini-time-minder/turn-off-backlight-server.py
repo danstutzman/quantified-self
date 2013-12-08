620ca5b369a8929900496fc2a25e8f8df9d1342b
Daniel Stutzman <dstutzman@dstutzman.(none)>
2013-12-07 19:23:06 -0700

    Automatically close blank-screen window instead of leaving bright white background to find the next morning.

--- dell-mini-time-minder/turn-off-backlight-server.py
+++ dell-mini-time-minder/turn-off-backlight-server.py
@@ -6,12 +6,13 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.end_headers()
-    self.wfile.write("ok")
+    self.wfile.write("<script>window.close();</script>")
     self.wfile.close()
     now = datetime.datetime.now()
     num_seconds = (5 - now.hour) * 3600 + (59 - now.minute) * 60
     if num_seconds < 0:
       num_seconds += 24 * 60 * 60
+    num_seconds = 3
     os.system('sleep 1 && xset dpms force off && sleep %d && xset dpms force on' % num_seconds)
 
 
